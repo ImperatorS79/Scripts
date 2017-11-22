b09370a121c265a90709d3cfb55384ecf1d8cddb
ImperatorS79 <fevrier.simon@gmail.com>
2017-11-22 11:20:36 +0100

    Added firefox

---
+++ Applications/Internet/Mozilla Firefox/Online/script.js
@@ -0,0 +1,23 @@
+include(["Engines", "Wine", "QuickScript", "OnlineScript"]);
+include(["Engines", "Wine", "Engine", "Object"]);
+include(["Utils", "Functions", "Filesystem", "Files"]);
+
+new OnlineScript()
+    .name("Mozilla Firefox")
+    .editor("The Mozilla Fundation")
+    .author("ImperatorS79")
+    .wineVersion(LATEST_STAGING_VERSION)
+    .wineDistribution("staging")
+    .category("Internet")
+    .executable("firefox.exe")
+    .url("https://download.mozilla.org/?product=firefox-latest&os=win&lang=")
+	.postInstall(function(wine, wizard) {
+        new Downloader()
+            .wizard(wizard)
+            .url("https://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player.exe")
+            .to(wine.prefixDirectory + "drive_c/users/install_flash_player.exe")
+			.get();
+			
+		wine.runInsidePrefix("/users/install_flash_player.exe");
+	})
+    .go();
