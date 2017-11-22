90dc9edbe0d8491baf414ed16bd0caf656ac368f
ImperatorS79 <fevrier.simon@gmail.com>
2017-11-22 22:38:19 +0100

    Update script.js

--- Applications/Internet/Mozilla Firefox/Online/script.js
+++ Applications/Internet/Mozilla Firefox/Online/script.js
@@ -10,7 +10,7 @@ new OnlineInstallerScript()
     .wineDistribution("staging")
     .category("Internet")
     .executable("firefox.exe")
-    .url("https://download.mozilla.org/?product=firefox-latest&os=win&lang=")
+    .url("https://download.mozilla.org/?product=firefox-latest&os=win")
     .postInstall(function(wine, wizard) {
         new Downloader()
             .wizard(wizard)
