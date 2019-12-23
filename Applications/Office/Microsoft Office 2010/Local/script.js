66c2bd51c56a1868bf01d3215710b536bafac131
ImperatorS79 <fevrier.simon@gmail.com>
2019-12-23 19:53:18 +0100

    Update script.js

--- Applications/Office/Microsoft Office 2010/Local/script.js
+++ Applications/Office/Microsoft Office 2010/Local/script.js
@@ -2,6 +2,12 @@ const LocalInstallerScript = include("engines.wine.quick_script.local_installer_
 
 const WineShortcut = include("engines.wine.shortcuts.wine");
 
+const Corefonts = include("engines.wine.verbs.corefonts");
+const DotNet20 = include("engines.wine.verbs.dotnet20");
+const MsXML6 = include("engines.wine.verbs.msxml6");
+const Riched20 = include("engines.wine.verbs.riched20");
+const GDIplusWinXP = include("engines.wine.verbs.gdiplus_winxp");
+
 const OverrideDLL = include("engines.wine.plugins.override_dll");
 
 new LocalInstallerScript()
@@ -10,6 +16,13 @@ new LocalInstallerScript()
     .author("ImperatorS79")
     .category("Office")
     // exe set with WineShorcut
+    .preInstall(function (wine) {
+        new Corefonts(wine).go();
+        new DotNet20(wine).go();
+        new MsXML6(wine).go();
+        new Riched20(wine).go();
+        new GDIplusWinXP(wine).go();
+    ))
     .postInstall(function (wine) {
         new OverrideDLL(wine).withMode("native, builtin", ["riched20"]).go();
 
